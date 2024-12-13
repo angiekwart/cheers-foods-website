46964c2002bae2d603d6b01975bb164a87dbec50
Angelica <naaodeangel@gmail.com>
2024-12-13 19:51:21 +0000

    Cheers Website
    Stage 1 complete

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import './Navbar'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Carousel from './Carousel'
+
+function App() {
+  
+  
+  return (
+    <>
+      <div>
+        <Carousel/>
+        <Navbar/>
+
+        
+        <Footer/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
